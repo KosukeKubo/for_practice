26c063bd95ee461420a3548578326ccf9108d336
KosukeKubo <kubo.kosuke.62f@st.kyoto-u.ac.jp>
2026-03-13 13:04:43 +0900

    Update news.py

--- news_api/news.py
+++ news_api/news.py
@@ -13,5 +13,5 @@ response = requests.get(url, params)
 data = response.json()
 
 for article in data["articles"][:5]:
-    print(article["title"]})
+    print(article["title"])
     print(article["url"])
