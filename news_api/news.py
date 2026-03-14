fc1069698014ce71a061dcd48c8db7b6cb287aa2
KosukeKubo <kubo.kosuke.62f@st.kyoto-u.ac.jp>
2026-03-14 16:03:34 +0900

    Update news.py

--- news_api/news.py
+++ news_api/news.py
@@ -1,17 +1,49 @@
 import requests
+import os
 
-url = "https://newsapi.org/v2/everything"
+def get_news(key_word: str="AI", n: int=5):
+    url = "https://newsapi.org/v2/everything"
+    NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+    
+    params = {
+        "q": key_word,
+        "searchIn": "title",
+        "sortBy": "publishedAt",
+        "apiKey": NEWS_API_KEY
+    }
+    
+    response = requests.get(url, params=params)
+    data = response.json()
 
-params = {
-    "q": "AI",
-    "searchIn": "title",
-    "sortBy": "publishedAt",
-    "apiKey":"c1c5efe25ddd4cd79cbaca8efdba7e9d"
-}
+    # Discordに送るための「1つのメッセージ（文字列）」を組み立てる
+    message = f"📢 今日の【{key_word}】ニュース上位{n}件をお届けします！\n\n"
+    
+    # data["articles"]が空の場合のエラー回避
+    articles = data.get("articles", [])
+    for article in articles[:n]:
+        title = article["title"]
+        url = article["url"]
+        message += f"🔹 {title}\n{url}\n\n"
 
-response = requests.get(url, params)
-data = response.json()
+    return message
 
-for article in data["articles"][:5]:
-    print(article["title"])
-    print(article["url"])
+def send_discord(message_text):
+    DISCORD_URL = os.environ.get("DISCORD_AI_NEWS_COLLECTOR_URL")
+    
+    data = {
+        "content": message_text
+    }
+    
+    response = requests.post(DISCORD_URL, json=data)
+    
+    if response.status_code == 204:
+        print("Discordへの送信に成功しました。")
+    else:
+        print(f"送信失敗: {response.status_code}")
+
+if __name__ == "__main__":
+    # 1. ニュースを取得して文字列にまとめる
+    content = get_news("AI", 5)
+    
+    # 2. まとめた文字列を送信する
+    send_discord(content)
